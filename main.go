0bb1291cb838224d2ae7185c9cb3ba38d67b62cc
Cerecero <ale.rey.cer@outlook.com>
2024-12-26 15:34:17 -0600

    added middlewareMetricsInc Func

--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+)
 
-type Server struct{
+type apiConfig struct{
+	fileserverHits atomic.Int32
+}
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler{
+	return http.HandlerFunc(func( w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main(){
 
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	server := http.Server{
+	mux.HandleFunc("/healthz", func( w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		
+		_,err := w.Write([]byte("OK"))
+		if err != nil {
+			panic(err)
+		}
+	})
+	fileserver := http.fileserver(http.dir("."))
+	mux.handle("/app/", http.stripprefix("/app", fileserver))
+
+
+	server := &http.Server{
 		Handler: mux,
 		Addr: ":8080",
 	}
-
-	server.ListenAndServe()
-
-	
-	// mux.Handle("/", http.FileServer(http.Dir(".")))
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 
 }
