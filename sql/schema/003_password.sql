77bcd2c40654362f24243460de31fade563594e4
Cerecero <ale.rey.cer@outlook.com>
2025-01-13 12:44:36 -0600

    changed the migration

--- sql/schema/003_password.sql
+++ sql/schema/003_password.sql
@@ -1,5 +1,3 @@
 -- +goose Up
 ALTER TABLE users
 ADD COLUMN hashed_password TEXT DEFAULT 'unset';
--- +goose Down
-DROP TABLE users;
--- sql/schema/004_revoke.sql
+++ sql/schema/004_revoke.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE refresh_tokens(token TEXT PRIMARY KEY, 
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id UUID NOT NULL REFERENCES USERS(id) ON DELETE CASCADE,
     expires_at TIMESTAMP NOT NULL,
     revoked_at TIMESTAMP
