025880213faf4212903366addba38ead25bea77e
Cerecero <ale.rey.cer@outlook.com>
2025-01-16 12:16:40 -0600

    fixed an issue with the query

--- internal/database/query_all.sql.go
+++ internal/database/query_all.sql.go
@@ -45,11 +45,11 @@ func (q *Queries) QueryChirp(ctx context.Context) ([]Chirp, error) {
 }
 
 const queryChirpById = `-- name: QueryChirpById :many
-SELECT id, created_at, updated_at, body, user_id FROM chirps WHERE id = $1 ORDER BY created_at ASC
+SELECT id, created_at, updated_at, body, user_id FROM chirps WHERE user_id = $1 ORDER BY created_at ASC
 `
 
-func (q *Queries) QueryChirpById(ctx context.Context, id uuid.UUID) ([]Chirp, error) {
-	rows, err := q.db.QueryContext(ctx, queryChirpById, id)
+func (q *Queries) QueryChirpById(ctx context.Context, userID uuid.UUID) ([]Chirp, error) {
+	rows, err := q.db.QueryContext(ctx, queryChirpById, userID)
 	if err != nil {
 		return nil, err
 	}
--- sql/queries/query_all.sql
+++ sql/queries/query_all.sql
@@ -2,4 +2,4 @@
 SELECT * FROM chirps ORDER BY created_at ASC;
 
 -- name: QueryChirpById :many
-SELECT * FROM chirps WHERE id = $1 ORDER BY created_at ASC;
+SELECT * FROM chirps WHERE user_id = $1 ORDER BY created_at ASC;
